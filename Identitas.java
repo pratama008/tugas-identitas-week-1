0be396aab5924ffdc252a86b7fcbeeeca84318a5
pratama008 <91895163+pratama008@users.noreply.github.com>
2023-03-08 22:39:24 +0700

    Add files via upload

---
+++ Identitas.java
@@ -0,0 +1,13 @@
+public class Identitas{
+    public static void main (String[] args){
+        String nama = "Pratama Rizqy Fauzan\n";
+        System.out.println("Nama saya adalah "+nama);
+        System.out.print(nama);
+        System.out.print("Nama      : Pratama Rizqy Fauzan\n");
+        System.out.print("NIM       : A11.2021.13444\n");
+        System.out.print("Jurusan   :Teknik Informatika\n");
+
+        int a,b,c;
+        float d,e;
+    }
+}
